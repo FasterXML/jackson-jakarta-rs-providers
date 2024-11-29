9db0db86282ae3645816a7fc9e1e16a87ca42eaf
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-11-29 14:31:57 -0800

    Changes wrt databind/3043 (SerializerProvider -> SerializationContext)

--- json/src/test/java/tools/jackson/jakarta/rs/json/dw/SimpleEndpointTestBase.java
+++ json/src/test/java/tools/jackson/jakarta/rs/json/dw/SimpleEndpointTestBase.java
@@ -57,43 +57,43 @@ public abstract class SimpleEndpointTestBase extends ResourceTestBase
         }
     }
 
-	protected static abstract class Page<E> {
+    protected static abstract class Page<E> {
 
-		public static final String PREV_PAGE_REL = "prev";
-		public static final String NEXT_PAGE_REL = "next";
+        public static final String PREV_PAGE_REL = "prev";
+        public static final String NEXT_PAGE_REL = "next";
 
-		public Link getPreviousPageLink() {
-			return getLink(PREV_PAGE_REL);
-		}
+        public Link getPreviousPageLink() {
+            return getLink(PREV_PAGE_REL);
+        }
 
-		public Link getNextPageLink() {
-			return getLink(NEXT_PAGE_REL);
-		}
+        public Link getNextPageLink() {
+            return getLink(NEXT_PAGE_REL);
+        }
 
-		public abstract List<E> getEntities();
-		public abstract Link getLink(String rel);
-	}
+        public abstract List<E> getEntities();
+        public abstract Link getLink(String rel);
+    }
 
-	@JsonPropertyOrder({ "entities", "links" })
-	@JsonAutoDetect(fieldVisibility = Visibility.ANY, creatorVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, isGetterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
-	protected static class PageImpl<E> extends Page<E> {
+    @JsonPropertyOrder({ "entities", "links" })
+    @JsonAutoDetect(fieldVisibility = Visibility.ANY, creatorVisibility = Visibility.ANY, getterVisibility = Visibility.NONE, isGetterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE)
+    protected static class PageImpl<E> extends Page<E> {
 
-		protected static class JsonLinkSerializer extends ValueSerializer<Link>
-		{
-			static final String HREF_PROPERTY = "href";
+        protected static class JsonLinkSerializer extends ValueSerializer<Link>
+        {
+            static final String HREF_PROPERTY = "href";
 
-			@Override
-			public void serialize(Link link, JsonGenerator g, SerializerProvider serializerProvider)
-			        throws JacksonException
-			{
-				g.writeStartObject();
-				g.writeStringProperty(HREF_PROPERTY, link.getUri().toString());
-				for (Entry<String, String> entry : link.getParams().entrySet()) {
-					g.writeStringProperty(entry.getKey(), entry.getValue());
-				}
-				g.writeEndObject();
-			}
-		}
+            @Override
+            public void serialize(Link link, JsonGenerator g, SerializationContext ctxt)
+                throws JacksonException
+            {
+                g.writeStartObject();
+                g.writeStringProperty(HREF_PROPERTY, link.getUri().toString());
+                for (Entry<String, String> entry : link.getParams().entrySet()) {
+                    g.writeStringProperty(entry.getKey(), entry.getValue());
+                }
+                g.writeEndObject();
+            }
+        }
 
 		protected static class JsonLinkDeserializer extends ValueDeserializer<Link>
 		{
